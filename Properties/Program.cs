e87e6fbf66beca766da99339455136d836625297
Angel Acosta <Mrnurdee@gmail.com>
2020-09-08 08:59:13 -0700

    Add project files.

---
+++ Properties/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joe Russo", "cookie");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+
+            
+            Console.WriteLine(avengers.Rating);
+        }
+    }
+}
